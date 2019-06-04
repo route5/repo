1183bcefff3fd71fb5f63d913efa8e0d9509f342
ユーザー000 <b17z6ms@aiit.ac.jp>
2019-06-04 08:09:18 +0000

    first commit to push

---
+++ same-test.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# same.shの機能
+# 引数が2じゃないとエラー    "input 2 argments"
+# 引数が自然数じゃなきゃエラー　"$1 or $2 is not natural number"
+# 一致してなかったら　　　"$1 is noto equal to $2"
+
+tmp=/tmp/$$
+
+ERROR_EXIT(){
+	echo "$1" 1>&2
+	rm -r ${tmp}*
+       exit 1	
+}
+
+# TEST1: input args
+echo "Plz input 2 argments" > ${tmp}-ans
+./same.sh 2> ${tmp}-result && ERROR_EXIT "error in test1-1" #引数なしで実行
+diff $tmp-ans $tmp-result || ERROR_EXIT "error in test1-2" # err masse比較
+
+./same.sh 1 2 3 2> ${tmp}-result && ERROR_EXIT "error in test1-3"
+diff $tmp-ans $tmp-result || ERROR_EXIT "error in test1-4" # err masse比較
+echo "test completed"
---
+++ same.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# same.shの機能
+# 引数が自然数じゃなきゃエラー　"$1 or $2 is not natural number"
+# 一致してなかったら　　　"$1 is noto equal to $2"
+
+# 引数の数が2じゃないとエラー　　"Plz input 2 argments"
+if [ $# -ne 2 ] ;
+then
+	echo "Plz input 2 argments" 1>&2
+	exit 1
+fi
+
+
+#本体
+if [ $1 -eq $2 ] ;
+then
+	echo "$1 is equal to $2"
+else
+	echo "$1 is not equal to $2"
+fi
+
+
+
